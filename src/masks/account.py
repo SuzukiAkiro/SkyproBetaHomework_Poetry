d901c7e2009761f5854e145f9fce06f9ad6e5567
Vladislav Vasilev <akiro_jpg@vk.com>
2023-10-21 00:23:07 +0300

    (refactor) Files reformated by black

--- src/masks/account.py
+++ src/masks/account.py
@@ -3,7 +3,9 @@ def account_mask(account_number: int) -> str:
     # Возможно не самое лучшее решение, но это все, что я смог придумать:(
     number = list(str(account_number))
     number_tail = number[-4:]
-    masked_number = ('*' * (len(number) - 4))
-    return masked_number + ''.join(number_tail)
+    masked_number = "*" * (len(number) - 4)
+    return masked_number + "".join(number_tail)
+
+
 if __name__ == "__main__":
-    print(account_mask(int(input('Введите номер счета \n'))))
+    print(account_mask(int(input("Введите номер счета \n"))))
