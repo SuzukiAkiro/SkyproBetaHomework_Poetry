d5eb0dbbb8536e8e3b3984b0d1f79b303c9d8fe5
Vladislav Vasilev <akiro_jpg@vk.com>
2023-10-21 20:01:41 +0300

    Updated config and changed funcs

--- src/masks/account.py
+++ src/masks/account.py
@@ -1,10 +1,13 @@
 def account_mask(account_number: int) -> str:
-    """Принимает номер счета и возвращает замаскированную версию"""
+    """
+    Принимает номер счета и возвращает замаскированную версию
+    :param account_number: Номер счета клиента
+    :return: Замаскированная версия
+    """
     # Возможно не самое лучшее решение, но это все, что я смог придумать:(
     number = list(str(account_number))
     number_tail = number[-4:]
-    masked_number = "*" * (len(number) - 4)
-    return masked_number + "".join(number_tail)
+    return "**" + "".join(number_tail)
 
 
 if __name__ == "__main__":
--- src/masks/card.py
+++ src/masks/card.py
@@ -1,5 +1,9 @@
 def card_mask(card_number: int) -> str:
-    """Принимает номер карты и возвращает замаскированую и обработаную версию"""
+    """
+    Принимает номер карты и возвращает замаскированую и обработаную версию
+    :param card_number: Номер для обработки
+    :return: Замаскированный номер
+    """
     # Превращаем номер карты в список строк для замены символов на '*'
     digit_list = list(str(card_number))
     digit_list[6:-4] = "*" * len(digit_list[6:-4])
@@ -8,8 +12,13 @@ def card_mask(card_number: int) -> str:
     for i in range(0, len(digit_list), 4):
         separated_list.extend([digit_list[i : i + 4]])
     separated_list = ["".join(item) for item in separated_list]
-    return " ".join(separated_list[:])
+    return " ".join(separated_list)
 
 
 if __name__ == "__main__":
-    print(card_mask(int(input("Введите 16-ти значный номер карты: \n"))))
+    card_number = int(input("Введите 16-ти значный номер карты: \n"))
+    if len(str(card_number)) != 16:
+        print("Вы ввели неверный номер карты!")
+        exit(0)
+    else:
+        print(card_mask(card_number))
