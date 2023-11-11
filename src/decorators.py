23dd5db67a65ebbfc2a37bf8606e5686f60e58b9
Vladislav Vasilev <akiro_jpg@vk.com>
2023-11-11 14:14:39 +0500

    Created log decorator

---
+++ src/decorators.py
@@ -0,0 +1,35 @@
+from functools import wraps
+from datetime import datetime
+
+
+def log(filename: str | None = None):
+    """
+    Декоратор, который регистрирует выполнение функции.
+    :param filename: Имя файла для ведения логов.
+    По умолчанию None - вывод в консоль.
+    :return:
+        inner: Декорированная функция.
+    """
+
+    def wrapper(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            if filename:
+                try:
+                    with open(filename, "a") as file:
+                        file.write(f"{datetime.now()} {func.__name__} ok!\n")
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    file.write(f"{datetime.now()} {func.__name__} error: {e} \n Inputs: {args}, {kwargs}\n")
+                    return func(*args, **kwargs)
+            else:
+                try:
+                    print(f"{datetime.now()} {func.__name__} ok!\n")
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"{datetime.now()} {func.__name__} error: {e} \n Inputs: {args}, {kwargs}\n")
+                    return func(*args, **kwargs)
+
+        return inner
+
+    return wrapper
