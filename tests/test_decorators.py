feaa839f7f25e8b21c7b677b698ba9539dc94009
Vladislav Vasilev <akiro_jpg@vk.com>
2023-11-12 17:41:39 +0500

    Rewrited correct tests for decorator func

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,15 +1,21 @@
+import os
+from datetime import datetime
+
 import pytest
 
 import src.decorators as decorators
 
 
 def test_log_success():
+    # Test when function works correctly
     @decorators.log(filename="log.txt")
     def test_function():
         return 42
 
-    # Test when function executes successfully
-    assert test_function() == 42
+    test_function()
+    with open("log.txt", "r") as file:
+        assert file.read() == f"{datetime.now().strftime('%Y-%m-%d %H:%M')} test_function ok!\n"
+        os.remove("log.txt")
 
 
 def test_log_failure():
@@ -21,3 +27,9 @@ def test_log_failure():
             raise Exception("Something went wrong")
 
         test_function2()
+        with open("log.txt", "r") as file:
+            assert (
+                file.read()
+                == f"{datetime.now().strftime('%Y-%m-%d %H:%M')} test_function Error: Something went wrong\n Inputs {''}, {''}\n"
+            )
+    os.remove("log.txt")
